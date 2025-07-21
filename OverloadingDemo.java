abadd26d9106fdf130923f03dbc8469462195ffc
ganga <gangav2028@cs.sjcetpalai.ac.in>
2025-07-21 14:28:39 +0530

    Add files via upload

---
+++ OverloadingDemo.java
@@ -0,0 +1,29 @@
+class Calculator{
+	public void add(int a,int b) {
+		int sum=a+b;
+		System.out.println("Sum: "+sum);
+	}
+	public void add(int a, int b,int c) {
+		int sum=a+b+c;
+		System.out.println("Sum: "+sum);
+	}
+	public void add(double a, double b) {
+		double sum=a+b;
+		System.out.println("Sum: "+sum);
+	}
+	public void add(String a,int b) {
+		String sum=a+b;
+		System.out.println("Concatenation: "+sum);
+		
+	}
+}
+public class OverloadingDemo {
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
+		calculator.add(6,5);
+		calculator.add(6,5,4);
+		calculator.add(6.5,5.5);
+		calculator.add("Hai",5);
+		
+	}
+}
